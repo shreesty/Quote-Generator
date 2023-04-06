d847d51e8b2d457ab1da5f353f8bf0be37b447c1
shreesty <shristi35dahal@gmail.com>
2023-04-06 19:28:19 +0545

    small change

--- script.js
+++ script.js
@@ -7,28 +7,28 @@ let quotes=[
     {
     
     quote:`"The only way to do great work is to love what you do."`,
-    person:`Steve Jobs`
+    person:`- Steve Jobs`
     },
 
     {
     quote:`"Success is not final, failure is not fatal: It is the courage to continue that counts."`,
-    person:`Winston Churchill`
+    person:`- Winston Churchill`
     },
     {
     quote:`"I have not failed. I've just found 10,000 ways that won't work"`,
-    person:`Thomas Edison`
+    person:`- Thomas Edison`
     },
     {
     quote:`"It does not matter how slowly you go as long as you do not stop."`,
-    person:`Confucius`
+    person:`- Confucius`
     },
     {
     quote:`"Believe you can and you're halfway there."`,
-    person:`Theodore Roosevelt`
+    person:`- Theodore Roosevelt`
     },
     {
     quote:`"If you want to live a happy life, tie it to a goal, not to people or things"`,
-    person:`Albert Einstein`
+    person:`- Albert Einstein`
     },
 ];
 
